becd258a1cad227fca8463c5c04bc10db1c50fa6
Eleanor Burgess <Eleanor.C.Burgess0940@gmail.com>
2020-05-08 17:17:56 -0400

    troubleshooting the heroku

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,7 +4,6 @@ module.exports = function (app) {
   app.get("/", function (req, res) {
     res.sendFile(path.join(_dirname, "../public/index.html"));
   });
-  console.log("hello");
 
   app.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/notes.html"));
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var path = require("path");
 var app = express();
 
-var PORT = process.env.PORT || 8080;
+var PORT = 3000;
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname + "assets/index.html"));
