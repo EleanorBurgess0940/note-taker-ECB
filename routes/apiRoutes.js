a0626305367e956cfc4b88ad6939036188207f91
Eleanor Burgess <Eleanor.C.Burgess0940@gmail.com>
2020-05-09 13:59:45 -0400

    troubleshooting errors

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -26,6 +26,17 @@ module.exports = function (app) {
       element.id = i + 1;
     });
   }
-
-  app.delete("/api/notes/:id");
+  // still working on making this work
+  app.delete("/api/notes/:id", function (req, res) {
+    const deleted = notesInfo.findIndex((i) => i.id == req.params.id);
+    notesInfo.splice(deleted, 1);
+    reWrite();
+    res.json(notesInfo);
+  });
+  let reWrite = () => {
+    let newNoteInfo = JSON.stringify(notesInfo);
+    fs.writeFile("db.json", newNoteInfo, (err) => {
+      if (err) throw err;
+    });
+  };
 };
